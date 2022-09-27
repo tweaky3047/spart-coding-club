e6296148e5192f356cd0a238b0985dbafaadf04f
tweaky3047 <yy304742@gmail.com>
2022-09-27 09:32:44 +0900

    쪽지시험

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Accesslog(models.Model):
+    class Meta:
+        db_table = "Access_log"
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=200)
